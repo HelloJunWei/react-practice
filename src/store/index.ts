881a0d773406a5e45d0a1e96d28af1e1eca1bcec
Neil Guo <kissme3131@gmail.com>
2022-02-16 23:04:02 +0800

    init

---
+++ src/store/index.ts
@@ -0,0 +1,44 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit"
+import { configureStore } from '@reduxjs/toolkit'
+import { Dispatch } from "react"
+
+export type InitState = {
+  count: number
+}
+
+const initialState: InitState = {
+  count: 0
+}
+
+
+export const slice = createSlice({
+  name: 'index',
+  initialState, 
+  reducers: {
+    increment: state => {
+      state.count += 1
+    },
+    decrement: state => {
+      state.count -= 1
+    },
+    addByValue: (state, action: PayloadAction<number>) => {
+      state.count += action.payload
+    }
+  }
+})
+
+export const { increment, decrement, addByValue } = slice.actions
+
+export const incrementAsync = (value: number) => (dispatch: Dispatch<any>) => {
+  setTimeout(() => {
+    dispatch(addByValue(value))
+  }, 1500)
+}
+
+
+
+export default configureStore({
+  reducer: {
+    counter: slice.reducer,
+  },
+});
