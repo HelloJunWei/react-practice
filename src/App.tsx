0c9c389c22e446033063ce70651d12b712adb7e3
lctech-Neil <kissme3131@gmail.com>
2022-02-17 17:06:13 +0800

    fix app structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import logo from './logo.svg'
 import './App.css'
 import { Routes, Route, Link } from 'react-router-dom'
 import Home from './views/Home'
@@ -6,14 +7,19 @@ import Counter from './views/Counter'
 function App() {
   return (
     <div className="App">
-      <nav>
-        <Link to="/home">home</Link> {'/'}
-        <Link to="/">main</Link>
-      </nav>
-      <Routes>
-        <Route path="home" element={<Home />} />
-        <Route path="/" element={<Counter />} />
-      </Routes>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello Vite + React!</p>
+        <nav>
+          <Link to="/home">home</Link> {'/'}
+          <Link to="/">main</Link>
+        </nav>
+        { /* router view */ }
+        <Routes>
+          <Route path="home" element={<Home />} />
+          <Route path="/" element={<Counter />} />
+        </Routes>
+      </header>
     </div>
   )
 }
--- src/views/Counter.tsx
+++ src/views/Counter.tsx
@@ -12,33 +12,29 @@ function Counter() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={ () => dispatch(increment())}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          <button type="button" onClick={ () => dispatch(incrementAsync(3))}>
-            add async
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <p>
+        <button type="button" onClick={ () => dispatch(increment())}>
+          count is: {count}
+        </button>
+      </p>
+      <p>
+        <button type="button" onClick={ () => dispatch(incrementAsync(3))}>
+          add async
+        </button>
+      </p>
+      <p>
+        Edit <code>App.tsx</code> and save to test HMR updates.
+      </p>
+      <p>
+        <a
+          className="App-link"
+          href="https://vitejs.dev/guide/features.html"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Vite Docs
+        </a>
+      </p>
     </div>
   )
 }
