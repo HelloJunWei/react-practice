4aa07a0b91c8e04887b6d4f395c2bf4192b7c28d
lctech-Neil <kissme3131@gmail.com>
2022-02-17 17:02:13 +0800

    add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,19 @@
-import logo from './logo.svg'
 import './App.css'
-import { InitState, increment, incrementAsync } from './store'
-import { useSelector, useDispatch } from 'react-redux'
+import { Routes, Route, Link } from 'react-router-dom'
+import Home from './views/Home'
+import Counter from './views/Counter'
 
 function App() {
-  const count = useSelector((state: { counter: InitState }) => {
-    return state.counter.count
-  })
-
-  const dispatch = useDispatch()
-
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={ () => dispatch(increment())}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          <button type="button" onClick={ () => dispatch(incrementAsync(3))}>
-            add async
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <nav>
+        <Link to="/home">home</Link> {'/'}
+        <Link to="/">main</Link>
+      </nav>
+      <Routes>
+        <Route path="home" element={<Home />} />
+        <Route path="/" element={<Counter />} />
+      </Routes>
     </div>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import store from './store/index'
+import { BrowserRouter } from "react-router-dom";
 import { Provider } from 'react-redux'
 
 ReactDOM.render(
   <Provider store={store}>
-    <React.StrictMode>
-    <App />
-    </React.StrictMode>,
+    <BrowserRouter>
+      <React.StrictMode>
+      <App />
+      </React.StrictMode>,
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 )
