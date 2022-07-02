c49294299abdf8ba28f7fbd5ee75f0ef288b5f0b
Neil Guo <kissme3131@gmail.com>
2022-07-02 11:29:44 +0800

    refactor: CustomLink

--- packages/shoppingCart/src/components/NavBar.tsx
+++ packages/shoppingCart/src/components/NavBar.tsx
@@ -1,4 +1,9 @@
-import { NavLink } from 'react-router-dom'
+import {
+  Link,
+  LinkProps,
+  useResolvedPath,
+  useMatch
+  } from 'react-router-dom'
 import navClass from '../assets/css/navBar.module.scss'
 import c from 'classnames'
 
@@ -14,9 +19,26 @@ export default function NavBar (props: { active: string }) {
   console.log('navBar')
   return (
     <div className="flex items-center py-5">
-      <NavLink className={`${classN} ${props.active === '/' ? navClass.active: ''}`} to="/">Home</NavLink>/
-      <NavLink className={`${classN} ${props.active === '/about' ? navClass.active: ''}`} to="/about">about</NavLink>/
-      <NavLink className={`${classN} ${props.active === '/store' ? navClass.active: ''}`} to="/store">store</NavLink>
+      <CustomLink to="/">Home</CustomLink>/
+      <CustomLink to="/about">about</CustomLink>/
+      <CustomLink to="/store">store</CustomLink>
     </div>
   )
+}
+
+
+function CustomLink({ children, to, ...props }: LinkProps) {
+  let resolved = useResolvedPath(to);
+  let match = useMatch({ path: resolved.pathname, end: true });
+  return (
+    <div>
+      <Link
+        className={`${classN} ${match ? navClass.active: ''}`}
+        to={to}
+        {...props}
+      >
+        {children}
+      </Link>
+    </div>
+  );
 }
