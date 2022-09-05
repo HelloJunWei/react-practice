da727854e7055bf8ccd829111f43fe7bf15e3e62
Neil Guo <kissme3131@gmail.com>
2022-09-05 23:46:26 +0800

    add user data hoc

---
+++ packages/chat/src/App.tsx
@@ -0,0 +1,27 @@
+import ChatClass from './chatClass'
+
+let init = false
+let chat: ChatClass | null = null
+if (!init) {
+  chat = new ChatClass({
+    css: ''
+  })
+  init = true
+}
+const open = () => {
+  chat?.triggerOpen(true)
+}
+
+function App() {
+
+  return (
+    <div className="m-5">
+      hello world <br />
+      <button className="bg-blue-600 text-white" onClick={() => open()}>
+        open
+      </button>
+    </div>
+  )
+}
+
+export default App
