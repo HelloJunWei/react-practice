1f60ef062ceb67874d851b5fb4547eb224cbf46f
Neil Guo <kissme3131@gmail.com>
2022-09-10 12:44:18 +0800

    move project to new repo

--- packages/chat/src/vite-env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vite/client" />
-interface ImportMetaEnv {
-  readonly VITE_APP_FIREBASE_API_KEY: string
-  readonly VITE_APP_FIREBASE_AUTH_DOMAIN: string
-  readonly VITE_APP_FIREBASE_STORAGE_BUCKET: string
-  readonly VITE_APP_FIREBASE_MESSAGING_SENDER_ID: string
-  readonly VITE_APP_FIREBASE_APP_ID: string
-  readonly VITE_APP_API_URL: string
-}
